f7fd6d395435ab76803194060290cb8d8575d604
Sadiqueali042 <Sadiqueali042@gmail.com>
2020-06-08 10:57:18 +0500

    child com updations

--- project10/src/App.js
+++ project10/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Dinner form './ dinner.j';
 function App() {
   return (
-    <div>Hellow World My Name is Sadiq.</div>
+    <div classeName="App"> <dinner/> Hellow World My Name is Sadiq.
+    </div>
   
   );
 }
---
+++ project10/src/dinner.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Dinner () {
+    return(
+        <div>
+            <h1> Today we are serving chicken biryani.</h1>
+            <h1> Today we are serving chiken Karahi.</h1>
+        </div>
+    )
+}
