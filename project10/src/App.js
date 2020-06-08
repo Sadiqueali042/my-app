245a51951b572c53445a564505f810cf298fd82e
Sadiqueali042 <Sadiqueali042@gmail.com>
2020-06-08 11:21:51 +0500

    Final
    But  not to create child components

--- project10/src/App.js
+++ project10/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Dinner form './ dinner.j';
+
 function App() {
   return (
-    <div classeName="App"> <dinner/> Hellow World My Name is Sadiq.
-    </div>
+    <div>Hellow World My Name is Sadiq.</div>
   
   );
 }
---
+++ project10/src/Dinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div> Today we are serving chiken baryani.</div>
+
+  
+  );
+}
+
+export default App;
